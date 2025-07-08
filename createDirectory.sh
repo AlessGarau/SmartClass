bc8b97d7135bef288ef5c6b9eb8cc186d48fa798
Alessandro Garau <aless@MacBook-Pro-de-Alessandro.local>
2025-07-08 15:49:50 +0200

    feat: route report by roomId and script to create directory ready to dev

---
+++ createDirectory.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <nom_du_dossier>"
+  exit 1
+fi
+
+DIR_NAME="$1"
+
+mkdir -p "$DIR_NAME/interface"
+
+touch "$DIR_NAME/interface/IInteractor.ts"
+touch "$DIR_NAME/interface/IRepository.ts"
+
+touch "$DIR_NAME/Controller.ts"
+touch "$DIR_NAME/Interactor.ts"
+touch "$DIR_NAME/Repository.ts"
+touch "$DIR_NAME/message.ts"
+touch "$DIR_NAME/Routes.ts"
+touch "$DIR_NAME/validate.ts"
+
+echo "Structure créée dans le dossier '$DIR_NAME'"
