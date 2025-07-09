d1e2148ff1d1ba0683e3326fc29283444cedee5b
Maria Petersen <maria.petersen@ealico.com>
2025-07-09 13:41:56 +0200

    ADD update type

--- packages/server/src/user/interface/IRepository.ts
+++ packages/server/src/user/interface/IRepository.ts
@@ -3,4 +3,5 @@ import { User } from "../../../database/schema/user";
 export interface IRepository {
   getUserByEmail(email: string): Promise<User | null>;
   createUser(email: string, password: string, first_name: string, last_name: string, role: "teacher" | "admin"): Promise<User>;
+  getUserById(id: string): Promise<User | null>;
 }
